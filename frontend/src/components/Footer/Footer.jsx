188ebefa09c884d8879e8b07b0200874c86c2815
sanjitxdutta <duttasanjit691@gmail.com>
2025-07-19 21:28:49 +0530

    Added footer section with logo and description

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/frontend_assets/assets'
+
+const Footer = () => {
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" />
+                    <p>Tomato is your daily dose of delicious. Whether you're craving classics or looking to explore something new, we connect you with the best local eats, all delivered with love and a splash of tomato-fresh goodness.</p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="" />
+                        <img src={assets.twitter_icon} alt="" />
+                        <img src={assets.linkedin_icon} alt="" />
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privecy policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>+91-1234-567890</li>
+                        <li>contact@tomato.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <p className="footer-copyright">Copyright 2025 © Tomato.com -All Right Reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
