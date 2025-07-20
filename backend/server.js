89e866dafb939cf2abac825d8a4a8d16c2c722cf
sanjitxdutta <duttasanjit691@gmail.com>
2025-07-20 20:08:02 +0530

    Added image upload functionality with multer and food item creation API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
 
 
 
@@ -15,10 +16,14 @@ app.use(cors())
 // db connection
 connectDB();
 
-app.get("/", (req, res)=>{
+//  api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+
+app.get("/", (req, res) => {
     res.send("API Working")
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`)
 })
