1df57008c526a31c3047a9b396fd19424d1b711b
sanjitxdutta <duttasanjit691@gmail.com>
2025-07-20 17:57:14 +0530

    Connected backend to MongoDB using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { connectDB } from './config/db.js';
 
 
 
@@ -10,6 +11,9 @@ const port = 4000;
 //  middleware
 app.use(express.json())
 app.use(cors())
+
+// db connection
+connectDB();
 
 app.get("/", (req, res)=>{
     res.send("API Working")
