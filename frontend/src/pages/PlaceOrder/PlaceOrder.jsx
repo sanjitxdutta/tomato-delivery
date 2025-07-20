ff1e7f2ba777d4c367878ba3f70655b0d1b43249
sanjitxdutta <duttasanjit691@gmail.com>
2025-07-20 16:45:48 +0530

    Fix: Hide delivery fee and total when cart is empty

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -37,12 +37,12 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>₹{2}</p>
+              <p>₹{getTotalCartAmount() === 0 ? 0 : 40}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>₹{getTotalCartAmount() + 2}</b>
+              <b>₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 40}</b>
             </div>
           </div>
           <button>PROCEED TO PAYMENT</button>
