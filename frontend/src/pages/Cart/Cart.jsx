848c892d61f4c4fb07b23599b82d58a4faa281cc
sanjitxdutta <duttasanjit691@gmail.com>
2025-07-20 16:35:57 +0530

    Created Place Order page with delivery form and cart summary

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react'
 import './cart.css'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
 
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   return (
     <div className='cart'>
@@ -56,7 +59,7 @@ const Cart = () => {
               <b>₹{getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
