0ff3d73b3d5a5a5fe7a8b55a5c844fb97d6fb3ca
Sanjit Dutta <110589185+sanjitxdutta@users.noreply.github.com>
2025-07-25 20:09:12 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000";
+    const url = "https://tomato-delivery-backend-62wn.onrender.com";
 
     const [token, setToken] = useState("");
 
